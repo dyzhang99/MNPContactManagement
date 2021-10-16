5123087a882d08f3a4341c3f93ea09ee18b14bca
Devin Zhang <dyzhang99@hotmail.com>
2021-10-16 14:11:15 -0400

    Modified fields of ContactDetail table

--- MNPContactManagementDB/table/ContactDetail.sql
+++ MNPContactManagementDB/table/ContactDetail.sql
@@ -2,11 +2,11 @@
 (
 	[ContactID] INT NOT NULL  IDENTITY, 
     [CustomerID] INT NOT NULL, 
-    [ContactName] VARCHAR(50) NOT NULL, 
+    [ContactName] VARCHAR(100) NOT NULL, 
     [JobTitle] VARCHAR(50) NOT NULL, 
     [Address] VARCHAR(200) NOT NULL, 
     [Phone] VARCHAR(50) NULL, 
-    [EmailAddress] VARCHAR(100) NULL, 
+    [EmailAddress] VARCHAR(150) NULL, 
     [Comments] VARCHAR(500) NULL, 
     [LastDateContacted] DATETIMEOFFSET NOT NULL, 
     CONSTRAINT [FK_ContactDetail_Customer] FOREIGN KEY (CustomerID) REFERENCES [Customer]([CustomerID]), 
