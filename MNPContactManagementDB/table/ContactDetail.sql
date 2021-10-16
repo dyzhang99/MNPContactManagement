e84d22bd783e241e974cac74899d46373980191a
Devin Zhang <dyzhang99@hotmail.com>
2021-10-16 13:46:15 -0400

    Created blank solution and added three projects (DB, Web, API) to the solution

---
+++ MNPContactManagementDB/table/ContactDetail.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[ContactDetail]
+(
+	[ContactID] INT NOT NULL  IDENTITY, 
+    [CustomerID] INT NOT NULL, 
+    [ContactName] VARCHAR(50) NOT NULL, 
+    [JobTitle] VARCHAR(50) NOT NULL, 
+    [Address] VARCHAR(200) NOT NULL, 
+    [Phone] VARCHAR(50) NULL, 
+    [EmailAddress] VARCHAR(100) NULL, 
+    [Comments] VARCHAR(500) NULL, 
+    [LastDateContacted] DATETIMEOFFSET NOT NULL, 
+    CONSTRAINT [FK_ContactDetail_Customer] FOREIGN KEY (CustomerID) REFERENCES [Customer]([CustomerID]), 
+    CONSTRAINT [PK_ContactDetail] PRIMARY KEY ([ContactID]),  
+)
---
+++ MNPContactManagementDB/table/Customer.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Customer]
+(
+	[CustomerID] INT NOT NULL  IDENTITY, 
+    [CustomerName] VARCHAR(100) NOT NULL, 
+    CONSTRAINT [PK_Customer] PRIMARY KEY ([CustomerID])
+)
